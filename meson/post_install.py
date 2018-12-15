6470a70b658741e58c32c291b066f702f42a8cca
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2018-12-15 13:47:31 +0900

    Add meson

---
+++ meson/post_install.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from os import environ, path
+from subprocess import call
+
+schemadir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'glib-2.0', 'schemas')
+iconchachedir = path.join(environ['MESON_INSTALL_PREFIX'], 'share', 'icons', 'hicolor')
+
+if not environ.get('DESTDIR'):
+    print('Compiling gsettings schemas…')
+    call(['glib-compile-schemas', schemadir])
+
+    print('Rebuilding desktop icon cache…')
+    call(['gtk-update-icon-cache', iconchachedir])
